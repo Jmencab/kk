242fcb6e2a4e77d72119df1e6f7efaf90d02e451
Julio <mendezcabrera@college.harvard.edu>
2017-04-14 17:29:58 -0400

    Random number and prepartitioning helper functions and tests implemented

--- kk.py
+++ kk.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 import sys
+import random 
+
 filename = sys.argv[1]
 num_file = open(filename, 'r')
 #to be used for 
 numlist = []
 for line in num_file:
 	numlist.append(int(line.split()[0]))
+
+#function for generating random instances of problem
+def gen_list(numlist=[]):
+	for _ in xrange(0,100):
+		numlist.append(random.randrange(1000000000000))
+	return numlist
 
 #Basic KK Algorithm; takes list and prints the residue
 def kar_karp(numlist):
@@ -16,11 +24,68 @@ def kar_karp(numlist):
 		diff = big - small 
 		numlist[len(numlist)-1] = diff 
 		numlist[len(numlist)-2] = 0
+	return numlist[len(numlist)-1]
+
+def kar_karp_test(numlist):
+	for i in range(len(numlist)):
+		numlist = sorted(numlist)
+		big = numlist[len(numlist)-1]
+		small = numlist[len(numlist)-2]
+		diff = big - small 
+		numlist[len(numlist)-1] = diff 
+		numlist[len(numlist)-2] = 0
 	print numlist[len(numlist)-1]
 
+#Function for running random moves
+def rand_move(numlist):
+	index_i = random.randrange(len(numlist))
+	index_j = random.randrange(len(numlist))
+	if(random.random() < .5):
+		numlist[index_i] = -numlist[index_i]
+	if(random.random() < .5):
+		numlist[index_j] = -numlist[index_j]
+	return numlist
+	print("a=%d,b=%d" % (f(x,n),g(x,n)))
+def rand_move_test(numlist):
+	"*****Original Numlist"
+	index_i = random.randrange(len(numlist))
+	print("Index_i: %d, Value: %d" % (index_i, numlist[index_i]))
+	index_j = random.randrange(len(numlist))
+	print("Index_j: %d, Value: %d" % (index_j, numlist[index_j]))
+	if(random.random() < .5):
+		numlist[index_i] = -numlist[index_i]
+	if(random.random() < .5):
+		numlist[index_j] = -numlist[index_j]
+	print numlist
+#Function for prepartitioning 
+def pre_part(numlist, prepart=[], aprime=[]):
+	for _ in xrange(len(numlist)):
+		prepart.append(random.randrange(0,len(numlist)))
+	for i in xrange(0,len(numlist)):
+		aprime.append(0)
+	for i in xrange(0, len(prepart)):
+		aprime[prepart[i]] += numlist[i]
+	return aprime
+
+#Function for prepartitioning 
+def pre_part_test(numlist, prepart=[], aprime=[]):
+	print "Original numlist:"
+	print numlist
+	for _ in xrange(len(numlist)):
+		prepart.append(random.randrange(0,len(numlist)))
+	print "Prepartition list:"
+	print prepart
+	for i in xrange(len(numlist)):
+		aprime.append(0)
+	for i in xrange(len(numlist)):
+		aprime[prepart[i]] += numlist[i]
+	print "New aprime"
+	print aprime
+	return aprime
 #invoke methods
 def main():
-    kar_karp(numlist)
+	random.seed(a=None)
+	print kar_karp(numlist)
 
 if __name__ == "__main__":
 	main()
--- tests.py
+++ tests.py
@@ -1,12 +1,59 @@
 #Test the rudimentary KK Algorithm
-import sys
-print sys.argv[1]
-from kk import kar_karp
+#from kk import kar_karp
+import kk
+#random.seed(a=None)
 example_one = [10,8,7,6,5]
 example_two = [4,2,3,2,1]
 example_three = [6,2,3,2,1]
-kar_karp(example_one)
-print("\n")
-kar_karp(example_two)
-print("\n")
-kar_karp(example_three)
+print "*******************************\n*******************************"
+print "Test 1.0. KK with 10,8,7,6,5\nExpect Residual 2"
+kk.kar_karp(example_one)
+print "Test 1.1. KK with 10,8,7,6,5\nExpect Residual 0"
+kk.kar_karp(example_two)
+print "Test 1.2. KK with 10,8,7,6,5\nExpect Residual 0"
+kk.kar_karp(example_three)
+print "*******************************\n*******************************"
+#test the random number generator
+rand_prob = kk.gen_list()
+print "Test 2.0. Run numlist generator. Expect 100 random numbers:"
+print rand_prob
+#Test the prepartition function
+print "*******************************\n*******************************"
+print "Test 3.0. Testing prepartitioning\n*****************************"
+kk.pre_part_test(example_one, [], [])
+print "Test 3.1. Testing prepartitioning\n*****************************"
+kk.pre_part_test(example_two, [], [])
+print "Test 3.2. Testing prepartitioning\n*****************************"
+kk.pre_part_test(example_three, [], [])
+
+print "*******************************\n*******************************"
+#test the random switch function
+print "Test 4.0 Testing Random Move"
+print "*******************************\n"
+#test the random switch function
+kk.rand_move_test(example_one)
+print "*******************************\n"
+kk.rand_move_test(example_one)
+print "*******************************\n"
+kk.rand_move_test(example_one)
+print "*******************************\n"
+kk.rand_move_test(example_one)
+print "Test 4.1 Testing Random Move"
+print "*******************************\n"
+kk.rand_move_test(example_two)
+print "*******************************\n"
+kk.rand_move_test(example_two)
+print "*******************************\n"
+kk.rand_move_test(example_two)
+print "*******************************\n"
+kk.rand_move_test(example_two)
+print "*******************************\n"
+print "Test 4.2 Testing Random Move"
+kk.rand_move_test(example_three)
+print "*******************************\n"
+kk.rand_move_test(example_three)
+print "*******************************\n"
+kk.rand_move_test(example_three)
+print "*******************************\n"
+kk.rand_move_test(example_three)
+print "*******************************\n"
